90349f7a758122761f9087a2c6e88a76f37e4b03
nouhoum2018 <40660005+nouhoum2018@users.noreply.github.com>
2025-09-05 19:28:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 @app.route('/shapiro', methods=['POST'])
 def shapiro_test():
     data = request.get_json()
-    values = data.get('residus', [])
+    residus = data.get('residus', [])
 
-    if not isinstance(values, list) or len(values) < 3:
-        return jsonify({'error': 'Liste de résidus invalide ou trop courte'}), 400
+    if not isinstance(residus, list) or len(residus) < 3:
+        return jsonify({'error': 'Liste invalide ou trop courte'}), 400
 
-    W, p = shapiro(values)
-
-    return jsonify({'W': W, 'p_value': p})
+    W, p_value = shapiro(residus)
+    return jsonify({'W': W, 'p_value': p_value})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
+
